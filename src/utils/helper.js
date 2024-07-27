23b92b8aabd04a2b018460e59b9869dc518a3a8a
SakshiRastogi1302 <sakshi.rastogi1302@gmail.com>
2024-07-27 12:45:55 +0530

    created a helper function that validates whether all fields in check flight status are being selected or not.
    If any field is not selected it will show a pop up.

---
+++ src/utils/helper.js
@@ -0,0 +1,32 @@
+export const allInputFieldsSelected = (airlineName, flightNo, date) => {
+    if (airlineName === "Select An Airline" && flightNo === "" && date === "mm/dd/yy") {
+        alert("Airline Name, Flight Number, Departure Date Is Required");
+    }
+    else if (airlineName === "Select An Airline" && date === "mm/dd/yy") {
+        alert("Airline Name & Departure Date Is Required");
+    }
+    else if (airlineName === "Select An Airline" && flightNo === "") {
+        alert("Airline Name & Flight Number Is Required");
+    }
+    else if (airlineName === "Select An Airline") {
+        alert("Airline Name Is Required");
+    }
+    else if (airlineName !== "Select An Airline") {
+        if (flightNo === "" && date === "mm/dd/yy") {
+            alert("Flight Number & Departure Date Is Required");
+        }
+        else if (flightNo === "") {
+            alert("Flight No Is Required");
+        }
+        else {
+            if (date === "mm/dd/yy") {
+                alert("Departure Date Is Required");
+            }
+            else {
+                return "success";
+            }
+        }
+    }
+
+    return "failed";
+}

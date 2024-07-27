73b5b514e36b9283b76d44d96bc2a5d24f57afe1
SakshiRastogi1302 <sakshi.rastogi1302@gmail.com>
2024-07-27 11:45:11 +0530

    Added comments and corrected the path of css file

--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import './css/index.css';
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
+  /**
+   * React.StrictMode helps us in indentifying potential problems in an application by activating additional checks and warnings for it's child component during development.
+   */
   <React.StrictMode>
+    {/* Calling the App Component. App Component is the root component that manages the application's main structure and renders other components. */}
     <App />
   </React.StrictMode>
 );

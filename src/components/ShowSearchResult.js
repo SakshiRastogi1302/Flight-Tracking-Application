425bf48bc78dca6d6313e7138f872a9357c268ad
SakshiRastogi1302 <sakshi.rastogi1302@gmail.com>
2024-07-27 16:12:11 +0530

    updated the fields based upon the mock data

--- src/components/ShowSearchResult.js
+++ src/components/ShowSearchResult.js
@@ -2,6 +2,8 @@ import React from 'react'
 import '../css/ShowSearchResult.css'
 
 const ShowSearchResult = ({ flightData }) => {
+  const departureTime = flightData.actual_departure? flightData.actual_departure.split("T")[1].split("Z")[0] : flightData.scheduled_departure.split("T")[1].split("Z")[0] ;
+  const arrivalTime = flightData.actual_arrival? flightData.actual_arrival.split("T")[1].split("Z")[0] : flightData.scheduled_arrival.split("T")[1].split("Z")[0] ;
   return (
     <div>
       {/* Created a container */}
@@ -10,26 +12,35 @@ const ShowSearchResult = ({ flightData }) => {
         <h1 className='checkFlightStatusHeading'>Searched Flight Status</h1>
         {/* Displaying the flight name and number */}
         <div className='flightNameAndNumber'>
-          <p className='name'>Airline Name :- {flightData.airlineName}</p>
-          <p className='flightNo'>Flight Number :- {flightData.flightNumber}</p>
+          <p className='name'>Airline Name :- {flightData.airline}</p>
+          <p className='flightNo'>Flight Number :- {flightData.flight_id}</p>
         </div>
         {/* Displaying the status of flight */}
         <div className='flightStatus'>
-          <p className='status'>Flight {flightData.currentFlightStatus}</p>
+          <p className='status'>Flight Status :- {flightData.status}</p>
         </div>
         {/* Created a horizontal line */}
         <hr className='horizontalLine'></hr>
         {/* Created a location container to display the arrival and departure location of flight */}
         <div className='location'>
-          {/* Arrival Container */}
-          <div>
-            <h1 className='arrival'>Arrival</h1>
-            <p className='arrivalLocation'>{flightData.arrivalLocation}</p>
-          </div>
           {/* Departure Container */}
           <div>
-            <h1 className='departure'>Departure</h1>
-            <p className='departureLocation'>{flightData.departureLocation}</p>
+            <h1 className='departure'>Departure Time</h1>
+            <p className='departureLocation'>{departureTime}</p>
+          </div>
+          <div>
+            <h1 className='departure'>Departure Gate</h1>
+            <p className='departureLocation'>{flightData.departure_gate}</p>
+          </div>
+
+          {/* Arrival Container */}
+          <div>
+            <h1 className='arrival'>Arrival Time</h1>
+            <p className='arrivalLocation'>{arrivalTime}</p>
+          </div>
+          <div>
+            <h1 className='arrival'>Arrival Gate</h1>
+            <p className='arrivalLocation'>{flightData.arrival_gate}</p>
           </div>
         </div>
       </div>

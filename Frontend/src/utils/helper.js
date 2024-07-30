762a103ab504e8f1fd6649a7239925d3ad5cd733
SakshiRastogi1302 <sakshi.rastogi1302@gmail.com>
2024-07-31 00:00:37 +0530

    added helper functions

--- Frontend/src/utils/helper.js
+++ Frontend/src/utils/helper.js
@@ -29,3 +29,53 @@ export const checkAllInputFieldsFilled = (airlineName, flightNo, date, fromLocat
     // All fields are filled
     return "true";
 };
+
+export const getEmailSubject = (status) => {
+    switch (status) {
+        case "On Time":
+            return "Flight On Time";
+        case "Delayed":
+            return "Flight Delayed";
+        case "Cancelled":
+            return "Flight Cancelled";
+        default:
+            return "Flight Departure Gate Is Changed";
+    }
+};
+
+export const getEmailBody = (flight) => {
+    switch (flight.status) {
+        case "On Time":
+            return `Your flight ${flight.flight_id} is on time. Departure gate: ${flight.departure_gate}.`;
+        case "Delayed":
+            return `Your flight ${flight.flight_id} is delayed. New departure time: ${flight.actual_departure}. Departure gate: ${flight.departure_gate}.`;
+        case "Cancelled":
+            return `Your flight ${flight.flight_id} has been cancelled.`;
+        default:
+            return `Your flight ${flight.flight_id} is on time. But the departure gate is changed to ${flight.departure_gate}.`;
+    }
+};
+
+
+export const sendEmail = async(flightInformation) => {
+    const emailList = JSON.parse(flightInformation.passengers_list);
+      for (const passenger in emailList) {
+        const flightInfo = {
+          email: emailList[passenger].emailId,
+          subject: getEmailSubject(flightInformation.status),
+          text: getEmailBody(flightInformation)
+        };
+
+        await fetch("http://localhost:8081/sendEmail", {
+          method: "POST",
+          headers: { 'Content-Type': 'application/json' },
+          body: JSON.stringify(flightInfo)
+        })
+      }
+}
+
+// Helper function to format time from ISO string
+export const formatTime = (timeString) => {
+    if (timeString === "null") return null;
+    return timeString.split("T")[1].split("Z")[0];
+  };

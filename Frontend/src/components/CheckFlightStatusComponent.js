c66fb18e0ecfd4bb70cb3acb41fd8e78991b58f6
SakshiRastogi1302 <sakshi.rastogi1302@gmail.com>
2024-07-30 13:38:06 +0530

    refactored the given code

--- Frontend/src/components/CheckFlightStatusComponent.js
+++ Frontend/src/components/CheckFlightStatusComponent.js
@@ -1,6 +1,6 @@
-import React, { useEffect, useState } from 'react'
-import '../css/CheckFlightStatusComponent.css'
-import searchIcon from '../images/search-icon.png'
+import React, { useEffect, useState } from 'react';
+import '../css/CheckFlightStatusComponent.css';
+import searchIcon from '../images/search-icon.png';
 import ShowSearchResultComponent from './ShowSearchResultComponent';
 import { checkAllInputFieldsFilled } from '../utils/helper';
 
@@ -9,135 +9,146 @@ const CheckFlightStatusComponent = () => {
     const [flightNo, setFlightNo] = useState("");
     const [date, setDate] = useState("mm/dd/yy");
     const [flightData, setFlightData] = useState(null);
+    const [fromLocation, setFromLocation] = useState("");
+    const [toLocation, setToLocation] = useState("");
     const [allInputFieldsFilled, setAllInputFieldsFilled] = useState(false);
 
     useEffect(() => {
         resetAllInputValues();
     }, []);
 
-    /**
-     * This function is used to update the value of airlineName state variable upon changing the input field value
-     * @param {*} e 
-     */
-    const updateAirLineName = (e) => {
-        setAirlineName(e.target.value);
-    }
+    const updateAirlineName = (e) => setAirlineName(e.target.value);
+    const updateFlightNo = (e) => setFlightNo(e.target.value);
+    const updateDate = (e) => setDate(e.target.value);
+    const updateFromLocation = (e) => setFromLocation(e.target.value);
+    const updateToLocation = (e) => setToLocation(e.target.value);
 
-    /**
-     * This function is used to update the value of flight number state variable upon changing the input field value
-     * @param {*} e 
-     */
-    const updateFlightNo = (e) => {
-        setFlightNo(e.target.value);
-    }
-
-    /**
-     * This function is used to update the value of date state variable upon changing the input field value.
-     * @param {*} e 
-     */
-    const updateDate = (e) => {
-        setDate(e.target.value);
-    }
-
-    /**
-     *  Set the value of all input field to default value
-     */
     const resetAllInputValues = () => {
-        setAirlineName("Select An Airline")
+        setAirlineName("Select An Airline");
         setFlightNo("");
         setDate("mm/dd/yy");
         setFlightData(null);
+        setFromLocation("");
+        setToLocation("");
         setAllInputFieldsFilled(false);
-    }
+    };
 
-    /**
-     * This function set the flight data if and only if all the input fields are selected.
-     */
     const findFlightDetails = async () => {
         const allInputFieldsSelected = checkAllInputFieldsFilled(airlineName, flightNo, date);
         setAllInputFieldsFilled(allInputFieldsSelected);
-        if (allInputFieldsSelected) {
-            const allFlightData = await (await fetch("http://localhost:8081/flight")).json();
-            const matchedFlightInfo = allFlightData.filter((flightData) => {
-                const departureDate = flightData.actual_departure === "null" ? flightData.scheduled_departure.split("T")[0] : flightData.actual_departure.split("T")[0];
-                return flightData.airline === airlineName && flightData.flight_id === flightNo && departureDate === date;
-            })
-            setFlightData(matchedFlightInfo[0]);
-            if(matchedFlightInfo[0] !== undefined){
-                const email = JSON.parse(matchedFlightInfo[0].passengersList);
-                for(let val in email){
-                    const flightInfo = {
-                        email: email[val]["emailId"],
-                        
-                        subject: matchedFlightInfo[0].status === "On Time"? "Flight On time" : matchedFlightInfo[0].status === "Delayed" ? "Flight is delayed" : matchedFlightInfo[0].status === "Cancelled" ? "Flight has been cancelled" : matchedFlightInfo[0].status === "Departure Gate Is Changed" ? "New departure gate is " +matchedFlightInfo[0].changed_departure_gate :  "New arrival gate is "+ matchedFlightInfo[0].changed_arrival_gate,
 
-                        text: matchedFlightInfo[0].status === "On Time"? "Your flight " + matchedFlightInfo[0].flight_id +" is on time. Departure gate: " + matchedFlightInfo[0].departure_gate+"." : matchedFlightInfo[0].status === "Delayed" ? "Your flight " + matchedFlightInfo[0].flight_id +" is delayed. New departure time: "+ matchedFlightInfo[0].actual_departure +". Departure gate: "+ matchedFlightInfo[0].departure_gate+".": matchedFlightInfo[0].status === "Cancelled" ? "Your flight "+ matchedFlightInfo[0].flight_id + " has been cancelled." : matchedFlightInfo[0].status === "Departure Gate Is Changed" ? "Your flight " + matchedFlightInfo[0].flight_id +" is on time. But the departure gate is changed from "+ matchedFlightInfo[0].departure_gate +" to "+ matchedFlightInfo[0].changed_departure_gate+"." :  "Your flight " + matchedFlightInfo[0].flight_id +" is on time. But the arrival gate is changed from "+ matchedFlightInfo[0].arrival_gate +" to "+ matchedFlightInfo[0].changed_arrival_gate+"."
+        if (allInputFieldsSelected) {
+            try {
+                const response = await fetch(`http://localhost:8081/flight?param1=${flightNo}&param2=${fromLocation}&param3=${toLocation}`);
+                const filteredFlightData = await response.json();
+                setFlightData(filteredFlightData[0]);
+
+                if (filteredFlightData[0]) {
+                    const emailList = JSON.parse(filteredFlightData[0].passengers_list);
+                    for (const passenger in emailList) {
+                        const flightInfo = {
+                            email: emailList[passenger].emailId,
+                            subject: getEmailSubject(filteredFlightData[0].status),
+                            text: getEmailBody(filteredFlightData[0])
+                        };
+
+                        await fetch("http://localhost:8081/sendEmail", {
+                            method: "POST",
+                            headers: { 'Content-Type': 'application/json' },
+                            body: JSON.stringify(flightInfo)
+                        });
                     }
-                    const options = {
-                        method: "POST",
-                        headers: {
-                            'Content-Type': 'application/json'
-                        },
-                        body: JSON.stringify(flightInfo)
-                    }
-                    await fetch("http://localhost:8081/sendEmail", options);
                 }
-                   
+            } catch (error) {
+                console.error("Error fetching flight details:", error);
             }
         }
-    }
+    };
+
+    const getEmailSubject = (status) => {
+        switch (status) {
+            case "On Time":
+                return "Flight On Time";
+            case "Delayed":
+                return "Flight Delayed";
+            case "Cancelled":
+                return "Flight Cancelled";
+            default:
+                return "Flight Status Update";
+        }
+    };
+
+    const getEmailBody = (flight) => {
+        switch (flight.status) {
+            case "On Time":
+                return `Your flight ${flight.flight_id} is on time. Departure gate: ${flight.departure_gate}.`;
+            case "Delayed":
+                return `Your flight ${flight.flight_id} is delayed. New departure time: ${flight.actual_departure}. Departure gate: ${flight.departure_gate}.`;
+            case "Cancelled":
+                return `Your flight ${flight.flight_id} has been cancelled.`;
+            default:
+                return `Your flight ${flight.flight_id} is on time. But the departure gate is changed to ${flight.departure_gate}.`;
+        }
+    };
 
     return (
-        // Created a search flight box container
-        <div className='searchFlightBox'>
-            {/* Created a box to check the status of flight by providing necessary details */}
-            <div className='flightStatusBox'>
-                {/* Added a heading */}
+        <div className='search-flight-box'>
+            <div className='flight-status-box'>
                 <p className='heading'>Check Flight Status</p>
-                {/* Added a table with heading and corresponding heading data */}
                 <table>
-                    {/* Added headings in table */}
                     <thead>
-                        <tr className='tableHeading'>
-                            <th className='airlineNameHeading'>Airline Name</th>
-                            <th className='flightNoHeading'>Flight No.</th>
-                            <th className='selectDateHeading'>Departure Date</th>
+                        <tr className='table-heading'>
+                            <th className='airline-name-heading'>Airline Name</th>
+                            <th className='flight-no-heading'>Flight No.</th>
+                            <th className='departure-date-heading'>Departure Date</th>
+                            <th className='from-location-heading'>From</th>
+                            <th className='to-location-heading'>To</th>
                         </tr>
                     </thead>
                     <tbody>
-                        {/* Added input fields for each heading.*/}
-                        <tr className='tableData'>
+                        <tr className='table-data'>
                             <td>
-                                <select className='airlineNamesInput' onChange={updateAirLineName} value={airlineName}>
-                                    <option value="Select An Airline" disabled={true}>Select An Airline</option>
+                                <select className='airline-name-input' onChange={updateAirlineName} value={airlineName}>
+                                    <option value="Select An Airline" disabled>Select An Airline</option>
                                     <option value="Indigo">Indigo</option>
                                     <option value="Air India">Air India</option>
                                 </select>
                             </td>
                             <td>
-                                <input type='text' className='flightNoInput' onChange={updateFlightNo} value={flightNo}></input>
+                                <input type='text' className='flight-no-input' onChange={updateFlightNo} value={flightNo} />
                             </td>
                             <td>
-                                <input type="date" className='dateInput' onChange={updateDate} value={date}></input>
+                                <input type="date" className='date-input' onChange={updateDate} value={date} />
+                            </td>
+                            <td>
+                                <input type='text' className='from-location-input' onChange={updateFromLocation} value={fromLocation} />
+                            </td>
+                            <td>
+                                <input type='text' className='to-location-input' onChange={updateToLocation} value={toLocation} />
                             </td>
                         </tr>
                     </tbody>
                 </table>
-                {/* Created a container for buttons.*/}
-                <div className='btns'>
-                    {/* Created a search button to find the status of the flight based upon the input.*/}
-                    <button className='searchFlightBtn' onClick={findFlightDetails}><img src={searchIcon} alt='Search' className='searchIcon' ></img>Show Flight Status</button>
-                    {/* Created a reset button that set the input field value with default value.*/}
-                    <button className='resetBtn' onClick={resetAllInputValues}>Reset</button>
+                <div className='buttons'>
+                    <button className='search-flight-btn' onClick={findFlightDetails}>
+                        <img src={searchIcon} alt='Search' className='search-icon' /> Show Flight Status
+                    </button>
+                    <button className='reset-btn' onClick={resetAllInputValues}>Reset</button>
                 </div>
             </div>
-            {/* If flight data exists for selected input field showcase the flight information.*/}
             {flightData ? (
-                <div className='searchResultBox'>
+                <div className='search-result-box'>
                     <ShowSearchResultComponent flightData={flightData} />
-                </div>) : allInputFieldsFilled ? <div className='noFlight'><h1>Sorry!! We cannot find any flight with the mentioned details.</h1></div> : null}
+                </div>
+            ) : allInputFieldsFilled === "true" ? (
+                <div className='no-flight'>
+                    <h1>We couldn't find any flights with the specified information.</h1>
+                </div>
+            ) : <div className='no-flight'>
+                <h1>Complete all the input fields to retrieve the flight status.</h1>
+            </div>}
         </div>
-    )
-}
+    );
+};
 
-export default CheckFlightStatusComponent
+export default CheckFlightStatusComponent;

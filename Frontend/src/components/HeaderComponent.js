741b44f236714aed2ec3e84c39969383549f3072
SakshiRastogi1302 <sakshi.rastogi1302@gmail.com>
2024-08-07 22:53:11 +0530

    created the header of the website with home, about, contact, booking terms and condition options and a sign in/sign up button

---
+++ Frontend/src/components/HeaderComponent.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import '../css/HeaderComponent.css'
+import airlineLogo from '../images/airline-logo.png'
+import { Link } from 'react-router-dom'
+
+const HeaderComponent = () => {
+  return (
+    <div className='header'>
+      <img src={airlineLogo} alt='Airline Logo' className='airline-logo'></img>
+      <ul className='navItems'>
+        <Link to='/' className='link'><li><i className="fa-solid fa-house"></i> Home</li></Link>
+        <Link to='/about' className='link'><li><i className="fa-solid fa-address-card"></i> About Us</li></Link>
+        <Link to='/bookingTerms' className='link'><li><i className="fa-solid fa-book-open-reader"></i> Booking Terms</li></Link>
+        <Link to='/contact' className='link'><li><i className="fa-solid fa-phone"></i> Get In Touch</li></Link>
+        <button className='signInBtn'><p className='signInBtnText'><i className="fa-solid fa-lock"></i> Sign In</p></button>
+      </ul>
+    </div>
+  )
+}
+
+export default HeaderComponent

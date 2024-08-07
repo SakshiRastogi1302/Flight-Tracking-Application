7b674a93db8a4fe932fc959211b935e2ada9a7de
SakshiRastogi1302 <sakshi.rastogi1302@gmail.com>
2024-08-07 22:50:36 +0530

    added route definitions to router instance and did some changes in existing router instance

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,30 +1,62 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import BodyComponent from "./components/BodyComponent";
-import CheckFlightStatusComponent from "./components/CheckFlightStatusComponent";
 import './css/App.css';
+import HomePageComponent from "./components/HomePageComponent";
+import ErrorComponent from "./components/ErrorComponent";
+import BookYourFlightComponent from "./components/BookYourFlightComponent";
+import AboutUsComponent from "./components/AboutUsComponent";
+import BookingTermsComponent from "./components/BookingTermsComponent";
+import ContactComponent from "./components/ContactComponent";
+import CheckFlightStatusComponent from './components/CheckFlightStatusComponent';
 
 
 function App() {
   /**
    * Created the router instance (appRouter) that manages navigation and routing in a browser environment, with the provided array typically containing route definitions.
    */
-  const appRouter = createBrowserRouter([
-    /**
-     * When the url of the website opened at local server is localhost:3000, BodyComponent will be rendered.
-     */
-    {
-     path:"/",
-     element:<BodyComponent />,
-    },
-
-    /**
-     * When the url of the website opened at local server is localhost:3000/checkFlightStatus, CheckFlightStatusComponent will be rendered.
-     */
-    {
-      path:"/checkFlightStatus",
-      element:<CheckFlightStatusComponent />
-    }
-  ])
+    const appRouter = createBrowserRouter([
+      /**
+       * When the url of the website opened at local server is localhost:3000, BodyComponent will be rendered.
+       */
+      {
+      path:"/",
+      element:<BodyComponent />,
+      children:[
+        {
+          path:"/",
+          element:<HomePageComponent />,
+          errorElement:<ErrorComponent />
+        },
+        {
+          path:"/about",
+          element:<AboutUsComponent />,
+          errorElement:<ErrorComponent />
+        },
+        {
+          path:"/bookingTerms",
+          element:<BookingTermsComponent />,
+          errorElement:<ErrorComponent />
+        },
+        {
+          path:"/contact",
+          element:<ContactComponent />,
+          errorElement:<ErrorComponent />
+        },
+        {
+          path:"/bookYourFlight",
+          element:<BookYourFlightComponent />,
+          errorElement:<ErrorComponent />
+        },
+        {
+          path:"/checkFlightStatus",
+          element:<CheckFlightStatusComponent />,
+          errorElement:<ErrorComponent />
+        }
+      ],
+      errorElement:<ErrorComponent />
+      }
+      
+    ])
   return (
     <div className="app">
       {/* <RouterProvider router={appRouter}> is used to provide the routing configuration defined by appRouter to the React application, enabling navigation and route management within the app based on the specified router instance. */}

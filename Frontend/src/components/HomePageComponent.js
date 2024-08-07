1331adf0951a0c37e454d6d94ef45ea6b12d2d93
SakshiRastogi1302 <sakshi.rastogi1302@gmail.com>
2024-08-07 22:53:51 +0530

    created the home page of the website

---
+++ Frontend/src/components/HomePageComponent.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import '../css/HomePageComponent.css'
+import backgroundImage from '../images/background-image.jpg'
+
+
+const HomePageComponent = () => {
+  return (
+    <div className='homepage'>
+          <div className='main-heading'>
+            <p className='homepage-heading'>Welcome To Sky Vista</p>
+            <p className='tagline'>Your Gateway to the Skies: Easy Booking, Endless Destinations</p>            
+          </div>
+          <div className='button'>
+            <Link to='/bookYourFlight'><button className='book-flight'>Book Your Flight</button></Link>
+            <Link to='/checkFlightStatus'><button className='check-status'>Check Flight Status</button></Link>
+          </div>
+          <div className='background-image'></div>
+    </div>
+  )
+}
+
+export default HomePageComponent

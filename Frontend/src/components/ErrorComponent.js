c7238344c2a17d7ac744c666b185bee04094d72a
SakshiRastogi1302 <sakshi.rastogi1302@gmail.com>
2024-08-16 23:15:57 +0530

    created error component

--- Frontend/src/components/ErrorComponent.js
+++ Frontend/src/components/ErrorComponent.js
@@ -1,8 +1,19 @@
 import React from 'react'
+import errorImage from '../images/error-image.png'
+import '../css/ErrorComponent.css'
+import {Link} from 'react-router-dom'
 
 const ErrorComponent = () => {
   return (
-    <div>ErrorComponent</div>
+    <div className='error-section'>
+      <img src={errorImage} alt='Error' className='error-logo'></img>
+      <div className='error-content'>
+        <h1 className='error-heading'>Error 404</h1>
+        <p className='error-text'>Oops! Page Not Found</p>
+        <div className='error-solution'>Sorry, the page you are looking for does not exist.</div>
+        <Link to='/'><button className='go-to-home-btn'><i className="fa-solid fa-house house-icon"></i> GO TO HOME</button></Link>
+      </div>
+    </div>
   )
 }
 

2e9f7a23ce1809efbdfd683868c2f62c9438ed89
SakshiRastogi1302 <sakshi.rastogi1302@gmail.com>
2024-08-27 21:05:24 +0530

    created code for login component

--- Frontend/src/components/LoginComponent.js
+++ Frontend/src/components/LoginComponent.js
@@ -1,8 +1,40 @@
-import React from 'react'
+import React, { useState } from 'react'
 import '../css/LoginComponent.css'
 
-export const LoginComponent = () => {
+const LoginComponent = () => {
+  const [signInOrSignOut, setSignInOrSignOut] = useState(true);
+
+  const toggleToSignInSignOut = () => {
+    setSignInOrSignOut(!signInOrSignOut);
+  }
+
   return (
-    <div>LoginComponent</div>
+    <div className='login-container'>
+      <div className='login-section'>
+        <h1 className='login-heading'>{signInOrSignOut ? "SIGN IN" : "SIGN UP"}</h1>
+        <div>
+          {signInOrSignOut ? null : (<div className='name-container'>
+            <label className='name-text'>Full Name</label>
+            <input type='text' className='name-input-field'></input>
+          </div>)}
+          <div className='email-container'>
+            <label className='email-text'>Email Address</label>
+            <input type='email' className='email-input-field'></input>
+          </div>
+          <div className='password-container'>
+            <label className='password-text'>Password</label>
+            <input type='password' className='password-input-field'></input>
+          </div>
+          <button className='signIn-btn'>Sign In</button>
+          <div className='create-account'>
+            <p className='create-account-text'>{signInOrSignOut ? "Don't have an account?" : "Already have an account"}</p>
+            <p className='register' onClick={toggleToSignInSignOut}>{signInOrSignOut ? "Sign Up" : "Sign In"}</p>
+          </div>
+          <hr className='horizontal-line'></hr>
+        </div>
+      </div>
+    </div>
   )
 }
+
+export default LoginComponent
